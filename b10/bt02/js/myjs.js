bd988fc8ce78fd98efa755e2df2ee42c23091f5b
tuanleqa <tuan.le@realstake.io>
2023-07-17 01:35:36 +0700

    game ngay 10

--- b10/bt02/js/myjs.js
+++ b10/bt02/js/myjs.js
@@ -1,18 +1,17 @@
-const url = 'https://5fe9340f2e12ee0017ab4d25.mockapi.io/course'
-
-$.get(url,
-    function (data) {
-        xhtml = '';
-        data.map((item, index) => {
-            xhtml += `<tr>
-					<th scope="row" class="id">${index}</th>
-					<td>${item.name}</td>
-					<td>${item.price}</td>
-					<td>${item.price_special}</td>
-				</tr>`
-        })
-        $(".table-body").html(xhtml);
-    },
-    "json"
-);
-
+$(document).ready(function() {
+  $('#open-popup-btn').click(function() {
+    $('#popup-overlay').fadeIn().addClass('open');
+    $('.number').addClass('animate-number');
+    setTimeout(function() {
+      $('.congratulations').addClass('animate-congratulations');
+    }, 1500);
+  });
+  
+  $('#popup-overlay').click(function(e) {
+    if (e.target.id === 'popup-overlay') {
+      $('#popup-overlay').fadeOut().removeClass('open');
+      $('.number').removeClass('animate-number');
+      $('.congratulations').removeClass('animate-congratulations');
+    }
+  });
+});
